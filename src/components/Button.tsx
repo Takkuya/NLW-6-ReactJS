9d2a07159dc2c2e235c5bddfb09cc9e1974c80c5
Takkuya <takuyagabriel@gmail.com>
2021-06-22 12:28:39 -0300

    Setup inicial e conexão com Firebase

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+type ButtonProps = {
+  text?: string
+}
+
+export function Button(props: ButtonProps) {
+  let [counter, setCounter] = useState(0)
+
+  function increment() {
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+
+  return <button onClick={increment}>{counter}</button>
+}
